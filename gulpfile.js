0260a8fec1b704317afb6e5df7f954213f0ad0d1
[JuanSCaicedo] <[sebanchit@gmail.com]>
2023-09-27 20:21:41 -0500

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,82 @@
+const { src, dest, watch, parallel } = require("gulp");
+
+//css
+const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
+
+//imagenes
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
+
+//JavaScript
+const terser = require('gulp-terser-js');
+
+function css(done) {
+    src('src/scss/**/*.scss') //Identificar arch de sass
+        .pipe(sourcemaps.init())
+        .pipe( plumber())
+        .pipe(sass()) //compilarlo
+        .pipe( postcss([ autoprefixer(), cssnano() ]) )
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest("build/css")) //almacenar en disco
+    done();
+}
+
+function imagenes(done) {
+    const opciones = {
+        optimizationLevel: 3
+    }
+    src('src/img/**/*.{png,jpg}')
+        .pipe( cache( imagemin(opciones) ) )
+        .pipe( dest('build/img') )
+    done();
+}
+
+function versionWebp(done) {
+    const opciones = {
+        quality: 50
+    }
+    src('src/img/**/*.{png,jpg}')
+        .pipe( webp(opciones) )
+        .pipe( dest('build/img') )
+    done();
+}
+
+function versionAvif(done) {
+    const opciones = {
+        quality: 50
+    }
+    src('src/img/**/*.{png,jpg}')
+        .pipe( avif(opciones) )
+        .pipe( dest('build/img') )
+    done();
+}
+
+function javascript(done) {
+    src('src/js/**/*.js')
+        .pipe( sourcemaps.init())
+        .pipe( terser())
+        .pipe( sourcemaps.write('.'))
+        .pipe(dest('build/js'));
+    done();    
+}
+
+function dev(done) {
+    watch('src/scss/**/*.scss', css);
+    watch('src/js/**/*.js', javascript);
+    done();
+}
+
+exports.css = css;
+exports.js = javascript;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev = dev;
+exports.dev = parallel( imagenes, versionWebp, versionAvif, javascript, dev);
